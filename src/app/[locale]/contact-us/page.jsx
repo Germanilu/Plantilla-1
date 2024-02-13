5ada467310784d7e57b4200d0ba6847922407222
Luciano Germani <lucianogermani93@gmail.com>
2024-02-13 21:18:47 +0100

    Feat: contact-us page

--- src/app/[locale]/contact-us/page.jsx
+++ src/app/[locale]/contact-us/page.jsx
@@ -1,14 +1,21 @@
 import { useTranslations }  from "next-intl";
 import Contact              from '../components/contact/index';
-
+import Navbar               from '../components/navbar/index';
 import './page.scss'
 
 export default function ContactUs(){
   const t = useTranslations("Contact-us");
   return (
     <div className="contact-us-design">
-      <h1>{t('title')}</h1>
+      <Navbar home={false}/>
+      <div className="contact-first-box"></div>
+      <div className="contact-img"></div>
+      <div className="contact-box">
+        <div className="information-contact-box">
+          text
+        </div>
       <Contact/>
+      </div>
     </div>
   ) 
 }
