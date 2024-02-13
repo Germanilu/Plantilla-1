618a38d47244ad3de9af60673736766266263d7b
Germanilu <lucianogermani93@gmail.com>
2024-02-13 23:29:09 +0100

    Feat: Contact us google map

--- src/app/[locale]/components/google-map-location/index.jsx
+++ src/app/[locale]/components/google-map-location/index.jsx
@@ -31,15 +31,97 @@ export default function Map(){
       const { Marker } = await loader.importLibrary('marker');
       
       const position = {
-        lat: 41.3917722839596,
-        lng: 2.1648703582061923
+        lat: 48.871412775556045,
+        lng: 2.303454843826624
       }
+      
 
       //map options
       const mapOptions = {
         center: position,
-        zoom: 17,
-        mapId: 'MY_NEXTJS_MAPID'
+        zoom: 15,
+        styles:[
+          { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+          { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+          { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+          {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry",
+            stylers: [{ color: "#263c3f" }],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#6b9a76" }],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#38414e" }],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#212a37" }],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#9ca5b3" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{ color: "#746855" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#1f2835" }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#f3d19c" }],
+          },
+          {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [{ color: "#2f3948" }],
+          },
+          {
+            featureType: "transit.station",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [{ color: "#17263c" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#515c6d" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#17263c" }],
+          },
+        ],
+        mapId: 'MY_NEXTJS_MAPID',
+        
       }
 
       //setup the map
--- src/app/[locale]/contact-us/page.jsx
+++ src/app/[locale]/contact-us/page.jsx
@@ -1,6 +1,8 @@
 import { useTranslations }  from "next-intl";
 import Contact              from '../components/contact/index';
 import Navbar               from '../components/navbar/index';
+import Map               from '../components/google-map-location/index';
+import { FaFacebookF, FaTwitter,FaInstagram }   from "react-icons/fa";
 import './page.scss'
 
 export default function ContactUs(){
@@ -12,9 +14,25 @@ export default function ContactUs(){
       <div className="contact-img"></div>
       <div className="contact-box">
         <div className="information-contact-box">
-          text
+          <span className="contact-main-title">We would love to hear from you</span>
+          <div className="phone-email">
+            <span>Tel: 666666666</span>
+            <span>info@mysite.com</span>
+          </div>
+          <div className="street">
+          <span className="text">{t('third-paragraph')}</span>
+          <span className="text">{t('fourth-paragraph')}</span>
         </div>
+        <div className="icons">
+            <FaTwitter size={17}/>
+            <FaFacebookF size={17}/>
+            <FaInstagram size={17}/>
+        </div>
+      </div>
       <Contact/>
+      </div>
+      <div className="google-maps-container">
+        <Map/>
       </div>
     </div>
   ) 
